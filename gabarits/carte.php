830efcb2b699bf1d3eafcc6482b55fac47ffbfa7
Tan <e6194865@cmaisonneuve.qc.ca>
2025-03-03 15:04:32 -0500

    Ajout de temperature

--- gabarits/carte.php
+++ gabarits/carte.php
@@ -16,5 +16,7 @@
     <h4 class="carte__titre"><?php the_title(); ?></h2>
     <p class="carte__description"><?php echo wp_trim_words(get_the_content(),10,"..."); ?></p>
     <a class = "carte__bouton carte__bouton--actif " href="<?php the_permalink()?>">suite ...</a>
+    <?php the_category(); ?>
+    <p>Température maximum : <?php the_field('temperature_maximum'); ?> C</p>
   </div>
 </article>
--- single-post.php
+++ single-post.php
@@ -11,6 +11,9 @@
                 ?>
                     <h2><?php the_title(); ?></h2>
                     <div><?php the_content(); ?></div>
+                    <p>Température maximum: <?php the_field('temperature_maximum') ?>C&#176;</p>
+                    <p>Température minimum: <?php the_field('temperature_minimum') ?>C&#176;</p>
+                    <p>Température moyenne: <?php the_field('temperature_moyenne') ?>C&#176;</p>
             </article>
                 <?php endwhile; endif; ?>
             </div>
