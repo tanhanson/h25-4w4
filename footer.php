a9a349157a825e69129d7418cd41c6be1f4f72d2
tanhanson <144844237+tanhanson@users.noreply.github.com>
2025-03-10 15:34:17 -0400

    s8c1 Modification des textes du footer avec la fonction wp_customize

--- footer.php
+++ footer.php
@@ -1,3 +1,10 @@
+<?php 
+$footer_adresse = get_theme_mod('footer_adresse', '5800 Sherbrooke-est Montréal (Québec) H1X 2A2');
+$footer_telephone = get_theme_mod('footer_telephone', '(514-254-7131)');
+$footer_mission = get_theme_mod('footer_mission', 'Notre mission est de inspirer et de faciliter vos voyages en vous offrant des expériences inoubliables. Que ce soit une escapade citadine, une aventure en pleine nature ou un séjour détente, nous vous guidons vers les meilleures destinations avec des conseils personnalisés et des offres exclusives. Voyagez mieux, explorez plus!');
+
+
+?>
 <footer>
 
 <div class="piedpage global" >
@@ -23,7 +30,8 @@
     <h3>Zone de recherche</h3>
     <div class="piedpage__s1__adresse__coord">
    
-    <p>3800 Rue Sherbrooke E, Montreal QC H1X2A2</p>
+    <p><?php echo $footer_adresse?></p>
+    <p>Téléphone : <?php echo $footer_telephone?></p>
     </div>
     <div class="piedpage__s1__adresse__recherche">
         <?php get_search_form(); ?>
@@ -32,7 +40,7 @@
     
     <div class="piedpage__s1__description">
     <h3>Lien sur le voyage</h3>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum mollitia voluptatum perferendis error atque earum optio blanditiis vero pariatur quaerat nesciunt eligendi quo dolor excepturi voluptatem eius, cum, quasi illo.
+        <p><?php echo $footer_mission?></p>
     </div>
 
 </section>
--- front-page.php
+++ front-page.php
@@ -1,6 +1,12 @@
 <?php get_header(); ?>
+<?php 
+$hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
+$hero_background = get_theme_mod('hero_background', '');
+$footer_addresse = get_theme_mod('')
 
-    <section class = "hero">
+?>
+
+    <section class = "hero" style="background-image: url(<?php echo $hero_background?>)">
             <div class="hero__contenu global">
                 <h1 class="hero__titre"><?php bloginfo('name')?></h1>
                 <p class="hero__description">
@@ -12,6 +18,7 @@
                 <p class="hero__adresse">
                     5800 Sherbrooke-est Montréal (Québec) H1X 2A2
                 </p>
+                <p class ="hero__auteur">Auteur : <?php echo $hero_auteur ?></p>
                 <div class="hero__icone">
                     <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
                     <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
