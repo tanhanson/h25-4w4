84298852e37eda941865c6b4b51952b820c7d255
Tan <e6194865@cmaisonneuve.qc.ca>
2025-03-12 15:59:50 -0400

    s8c2: Simplification du fichier functions dans les fichiers options.php et customizer.php

--- functions.php
+++ functions.php
@@ -1,129 +1,15 @@
 <?php
+// Chemin vers le dossier functions
+$functions_dir = get_template_directory() . '/functions/';
 
-function theme_4w4_customize_register($wp_customize) {
-  // Le code pour ajouter des sections, des réglages et des contrôles ira ici.
-  // Création d'une novuelle section dans le customizer
-  $wp_customize->add_section('hero_section', array(
-    'title' => __('Section Hero', 'theme_4w4'),
-    'priority' => 30,
-));
+// Liste des fichiers à inclure
+$function_files = array(
+    'customizer.php',
+    'options.php',
+    
+);
 
-////////////////////////////////// ajout de la donnée
-$wp_customize->add_setting('hero_auteur', array(
-  'default' => __('Hanson Tan', 'theme_4w4'),
-  'sanitize_callback' => 'sanitize_text_field'
-));
-
-////////////////////////////////// ajout du contrôle de la donnée
-$wp_customize->add_control('hero_auteur', array(
-  'label' => __('Auteur', 'theme_4w4'),
-  'section' => 'hero_section',
-  'type' => 'text',
-
-));
-
-/////////////////////////////// footer
-
-$wp_customize->add_section('footer_section', array(
-  'title' => __('Section Footer', 'theme_4w4'),
-  'priority' => 30,
-));
-$wp_customize->add_setting('footer_adresse', array(
-  'default' => __('5800 Sherbrooke-est Montréal (Québec) H1X 2A2', 'theme_4w4'),
-  'sanitize_callback' => 'sanitize_text_field'
-));
-
-$wp_customize->add_control('footer_adresse', array(
-  'label' => __('Adresse', 'theme_4w4'),
-  'section' => 'footer_section',
-  'type' => 'text',
-
-));
-
-$wp_customize->add_setting('footer_telephone', array(
-  'default' => __('(514-254-7131)', 'theme_4w4'),
-  'sanitize_callback' => 'sanitize_text_field'
-));
-
-$wp_customize->add_control('footer_telephone', array(
-  'label' => __('Telephone', 'theme_4w4'),
-  'section' => 'footer_section',
-  'type' => 'text',
-
-));
-
-
-$wp_customize->add_setting('footer_mission', array(
-  'default' => __('Notre mission est de', 'theme_4w4'),
-  'sanitize_callback' => 'sanitize_text_field'
-));
-
-$wp_customize->add_control('footer_mission', array(
-  'label' => __('Mission', 'theme_4w4'),
-  'section' => 'footer_section',
-  'type' => 'text',
-
-));
-////////////////////////////////// ajout de la donnée image en arrière-plan
-
-$wp_customize->add_setting('hero_background', array(
-  'default' => '',
-  'sanitize_callback' => 'esc_url_raw',
-));
-
-////////////////////////////////// ajout du contrôle de la donnée
-
-$wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_background', array(
-  'label' => __('Image en arrière plan', 'theme_4w4'),
-  'section' => 'hero_section',
-)));
-
+// Boucle pour inclure tous les fichiers
+foreach ($function_files as $file) {
+    include_once $functions_dir . $file;
 }
-
-add_action('customize_register', 'theme_4w4_customize_register');
-
-
-
-function mon_theme_supports() {
- 
-  add_theme_support("title-tag");
-  add_theme_support("menus");
-  add_theme_support("post-thumbnails");
-
-  add_theme_support("custom-logo", array(
-    'height'     => 250,
-    'width'       => 250,
-    'flex-height' => true,
-    'flex-width'  => true,
-));
-}
-
-add_action( 'after_setup_theme', 'mon_theme_supports' );
-
-
-
-function theme_4w4_enqueue_styles() { 
-wp_enqueue_style('normalize', get_template_directory_uri() . '/normalize.css'); 
-wp_enqueue_style('mon-style-style', get_stylesheet_uri()); 
-}
-/*
-
-*/
-add_action('wp_enqueue_scripts', 'theme_4w4_enqueue_styles');
-
-/**
- * Modifie la requete principale de WordPress avant qu'elle soit exécuté
- * le hook « pre_get_posts » se manifeste juste avant d'exécuter la requête principal
- * Dépendant de la condition initiale on peut filtrer un type particulier de requête
- * Dans ce cas ci nous filtrons la requête de la page d'accueil
- * @param WP_query  $query la requête principal de WP
- */
-function modifie_requete_principal( $query ) {
-    if ( $query->is_home() && $query->is_main_query() && ! is_admin() ) {
-      $query->set( 'category_name', 'populaire' );
-      $query->set( 'orderby', 'title' );
-      $query->set( 'order', 'ASC' );
-      }
-     }
-     add_action( 'pre_get_posts', 'modifie_requete_principal' );
-?>
---
+++ functions/customizer.php
@@ -0,0 +1,85 @@
+<?php
+
+function theme_4w4_customize_register($wp_customize) {
+    // Le code pour ajouter des sections, des réglages et des contrôles ira ici.
+    // Création d'une novuelle section dans le customizer
+    $wp_customize->add_section('hero_section', array(
+      'title' => __('Section Hero', 'theme_4w4'),
+      'priority' => 30,
+  ));
+  
+  ////////////////////////////////// ajout de la donnée
+  $wp_customize->add_setting('hero_auteur', array(
+    'default' => __('Hanson Tan', 'theme_4w4'),
+    'sanitize_callback' => 'sanitize_text_field'
+  ));
+  
+  ////////////////////////////////// ajout du contrôle de la donnée
+  $wp_customize->add_control('hero_auteur', array(
+    'label' => __('Auteur', 'theme_4w4'),
+    'section' => 'hero_section',
+    'type' => 'text',
+  
+  ));
+  
+  /////////////////////////////// footer
+  
+  $wp_customize->add_section('footer_section', array(
+    'title' => __('Section Footer', 'theme_4w4'),
+    'priority' => 30,
+  ));
+  $wp_customize->add_setting('footer_adresse', array(
+    'default' => __('5800 Sherbrooke-est Montréal (Québec) H1X 2A2', 'theme_4w4'),
+    'sanitize_callback' => 'sanitize_text_field'
+  ));
+  
+  $wp_customize->add_control('footer_adresse', array(
+    'label' => __('Adresse', 'theme_4w4'),
+    'section' => 'footer_section',
+    'type' => 'text',
+  
+  ));
+  
+  $wp_customize->add_setting('footer_telephone', array(
+    'default' => __('(514-254-7131)', 'theme_4w4'),
+    'sanitize_callback' => 'sanitize_text_field'
+  ));
+  
+  $wp_customize->add_control('footer_telephone', array(
+    'label' => __('Telephone', 'theme_4w4'),
+    'section' => 'footer_section',
+    'type' => 'text',
+  
+  ));
+  
+  
+  $wp_customize->add_setting('footer_mission', array(
+    'default' => __('Notre mission est de', 'theme_4w4'),
+    'sanitize_callback' => 'sanitize_text_field'
+  ));
+  
+  $wp_customize->add_control('footer_mission', array(
+    'label' => __('Mission', 'theme_4w4'),
+    'section' => 'footer_section',
+    'type' => 'text',
+  
+  ));
+  ////////////////////////////////// ajout de la donnée image en arrière-plan
+  
+  $wp_customize->add_setting('hero_background', array(
+    'default' => '',
+    'sanitize_callback' => 'esc_url_raw',
+  ));
+  
+  ////////////////////////////////// ajout du contrôle de la donnée
+  
+  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'hero_background', array(
+    'label' => __('Image en arrière plan', 'theme_4w4'),
+    'section' => 'hero_section',
+  )));
+  
+  }
+  
+  add_action('customize_register', 'theme_4w4_customize_register');
+
+  ?>
