b010cc1bdb17ac75bc47e39c22d85f2dcd6cf524
Tan <e6194865@cmaisonneuve.qc.ca>
2025-03-12 16:27:26 -0400

    s8c2: Simplification du site web en utilisant get_template_part pour les icones, ajout du fichier icones.php

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <?php 
-$hero_auteur = get_theme_mod('hero_auteur', 'Default Title');
+$hero_auteur = get_theme_mod('hero_auteur', 'Hanson Tan');
 $hero_background = get_theme_mod('hero_background', '');
 $footer_addresse = get_theme_mod('');
 $hero_couleur = get_theme_mod('hero_couleur', '');
@@ -21,11 +21,7 @@ $hero_couleur = get_theme_mod('hero_couleur', '');
                 </p>
                 <p class ="hero__auteur">Auteur : <?php echo $hero_auteur ?></p>
                 <div class="hero__icone">
-                    <img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
-                    <img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
-                    <img src="https://s2.svgbox.net/social.svg?ic=wordpress&color=000000" width="20" height="20">
-                    <img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000000" width="20" height="20">
-                    <img src="https://s2.svgbox.net/social.svg?ic=discord&color=000000" width="20" height="20">
+                   <?php get_template_part('gabarits/icones'); ?>
                 </div>
 
                 <button class = "hero__bouton" type = "submit"> S'INSCRIRE</button>
---
+++ gabarits/icones.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+
+<img src="https://s2.svgbox.net/social.svg?ic=facebook&color=000000" width="20" height="20">
+<img src="https://s2.svgbox.net/social.svg?ic=linkedin&color=000000" width="20" height="20">
+<img src="https://s2.svgbox.net/social.svg?ic=wordpress&color=000000" width="20" height="20">
+<img src="https://s2.svgbox.net/social.svg?ic=snapchat&color=000000" width="20" height="20">
+<img src="https://s2.svgbox.net/social.svg?ic=discord&color=000000" width="20" height="20">
