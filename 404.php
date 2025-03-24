01a9aaa2ee2bee392809f047417549ef86f1d066
tanhanson <144844237+tanhanson@users.noreply.github.com>
2025-03-24 14:54:00 -0400

    s9c1: Ajout du bouton pour retourner à l'accueil

--- 404.php
+++ 404.php
@@ -1,12 +1,18 @@
 <?php
+    $hero_404 = get_theme_mod('hero_erreur', '404');
     $hero_erreur = get_theme_mod('hero_erreur', 'Oops ! On dirait que tu tes perdu dans le cyberespace... ');
-    $hero_solution = get_theme_mod('hero_erreur', 'Ne t`inquiète pas, tu peux toujours retourner à la maison en cliquant sur le logo du site!' );
+    $hero_solution = get_theme_mod('hero_erreur', 'Ne t`inquiète pas, tu peux toujours retourner à la maison en cliquant sur ce bouton!' );
 ?>
 <?php get_header(); ?>
-<h1>404</h1>
-<p> <?php echo $hero_erreur ?></p>
-<p> <?php echo $hero_solution?></p>
-<img src="<?php echo esc_url(get_theme_mod('hero_imageErreur')); ?>" alt="Image d'erreur">
+
+<div class="conteneur-erreur">
+        <h1><?php echo $hero_404; ?></h1>
+        <p><?php echo $hero_erreur; ?></p>
+        <p><?php echo $hero_solution; ?></p>
+        <img class = "image-erreur" src="<?php echo esc_url(get_theme_mod('hero_imageErreur')); ?>" alt="Image d'erreur">
+        <br>
+        <a href="<?php echo home_url(); ?>" class="bouton-accueil">Retourner à l'accueil</a>
+    </div>
 
  
    
